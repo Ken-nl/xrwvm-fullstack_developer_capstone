703b3d5f2a2aca0d74bd6528de585a4d7edb9e64
Ken-nl <neutrallife.arttherapy@gmail.com>
2025-07-09 03:06:29 -0400

    odify files

--- server/frontend/src/App.js
+++ server/frontend/src/App.js
@@ -1,11 +1,14 @@
+import React from "react";
 import LoginPanel from "./components/Login/Login"
+import RegisterContainer from "./components/Register/Register"
 import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/login" element={<LoginPanel />} />
-    </Routes>
+        <Routes>
+            <Route path="/login" element={<LoginPanel />} />
+            <Route path="/register" element={<RegisterContainer />} />
+        </Routes>
   );
 }
 export default App;
